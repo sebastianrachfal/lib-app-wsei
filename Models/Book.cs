2738a5f37ac785ad391cb38fd1404113d0da3aac
sebastianrachfal <36996602+sebastianrachfal@users.noreply.github.com>
2022-02-26 16:25:40 +0100

    Add ex 2

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,17 +9,22 @@ namespace LibApp.Models
   public class Book
   {
     public int Id { get; set; }
-    [Required]
+
+    [Required(ErrorMessage = "Name is required.")]
     [StringLength(255)]
     public string Name { get; set; }
-    [Required]
+    [Required(ErrorMessage = "AuthorName is required.")]
     public string AuthorName { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Genre is required.")]
     public Genre Genre { get; set; }
     public byte GenreId { get; set; }
     public DateTime DateAdded { get; set; }
+    [Required(ErrorMessage = "ReleaseDate is required.")]
     public DateTime ReleaseDate { get; set; }
+    [Required(ErrorMessage = "NumberInStock is required.")]
+    [Range(1, 20, ErrorMessage = "NumberInStock must be between 1 and 20")]
     public int NumberInStock { get; set; }
+    [Required(ErrorMessage = "NumberAvailable is required.")]
     public int NumberAvailable { get; set; }
   }
 
